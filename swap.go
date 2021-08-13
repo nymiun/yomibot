1f4113c70bac6502e7ccaa787efe9882f1f16c2c
Juan Alvarez <juan@nemphi.com>
2021-08-13 15:18:02 -0500

    Basic error messages

--- swap.go
+++ swap.go
@@ -15,16 +15,19 @@ func (a *agata) swap(bot *sento.Bot, info sento.HandleInfo) error {
 	args := strings.Split(info.MessageContent, " ")
 	if len(args) < 2 {
 		// TODO: send err msg
+		bot.Send(info, "Not enough args")
 		return nil
 	}
 	a1, err := strconv.Atoi(args[0])
 	if err != nil {
-		// TODO: send err msg
+		// TODO: make prettier
+		bot.Send(info, "a1 is not number")
 		return nil
 	}
 	a2, err := strconv.Atoi(args[1])
 	if err != nil {
-		// TODO: send err msg
+		// TODO: make prettier
+		bot.Send(info, "a2 is not number")
 		return nil
 	}
 	gs := gsi.(*guildState)
